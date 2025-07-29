5108f7c88d56f3c8ff41139333e9629039b92df0
hosseinka6566 <hosseinka6566@gmail.com>
2025-07-29 13:06:42 +0330

    mizban

---
+++ assets/vendors/mizban/grapesJs/js/editor/editor.js
@@ -0,0 +1,30 @@
+import { editor_assetsManager, setupAssetsManager, editor_panelManager, code_editor, plugins } from "../../Controller.js";
+import { initializeWidgets } from '../widget/widget-setup.js';
+
+const editor = grapesjs.init({
+    container: "#canvas",
+    fromElement: true,
+    height: "100vh",
+    draggable: true,
+    plugins: plugins,
+    selectorManager: { componentFirst: true },
+    storageManager: true,
+});
+
+editor_assetsManager(editor);
+setupAssetsManager(editor);
+editor_panelManager(editor);
+code_editor(editor);
+
+initializeWidgets(editor);
+
+
+editor.on('load', () => {
+    setTimeout(() => {
+        editor.runCommand('open-blocks');
+        const blocksButton = editor.Panels.getButton('views', 'open-blocks');
+        if (blocksButton) {
+            blocksButton.set('active', true);
+        }
+    }, 300);
+});
