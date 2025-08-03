8d2d3c5472cceecb9405a087a8ebfaee349f7f73
hosseinka6566 <hosseinka6566@gmail.com>
2025-08-03 15:12:55 +0330

    modify responsive in grapesjs

--- assets/vendors/mizban/grapesJs/js/commands/commands.js
+++ assets/vendors/mizban/grapesJs/js/commands/commands.js
@@ -1,4 +1,5 @@
 import { breakPoints } from "../../../commands/variables.js";
+
 function commands(editor) {
     Object.keys(breakPoints).forEach(key => {
         editor.Commands.add(`set-device-${key}`, {
@@ -6,7 +7,6 @@ function commands(editor) {
         });
     });
 }
-
 
 
 export  {commands}
--- assets/vendors/mizban/grapesJs/js/devices/device-manager.js
+++ assets/vendors/mizban/grapesJs/js/devices/device-manager.js
@@ -1,12 +1,16 @@
 import { breakPoints } from "../../../commands/variables.js";
+
 function device_Manager(editor) {
     Object.keys(breakPoints).forEach(key => {
-        const breakPointsName = breakPoints[key];
+        const originalValue = breakPoints[key];
+        const numericValue = parseInt(originalValue, 10);
+        const adjustedValue = (numericValue - 1) + 'px';
+        
         try {
             editor.Devices.add({
                 id: key,
                 name: key,
-                width: breakPoints[key],
+                width: adjustedValue,
             })
         } catch (error) {}
     });
